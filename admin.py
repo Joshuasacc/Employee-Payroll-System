cb9e01f0f820a0279440245c09570262972a6b45
Joshua Ruiz <bossmasster100@gmail.com>
2025-04-18 16:41:05 +0800

    rebase code for admin

--- admin.py
+++ admin.py
@@ -20,7 +20,12 @@ def Error(error_Name):
     print("_________________________________________")
     print(f"\n   {error_Name}!! Press Enter to try again")
     error = input("_________________________________________\n")
-                                                                                                                                                                                                 
+
+def PrintError(error_name):
+    Error(error_name)
+    CLearScreen()
+    Introduction()
+                                                                                                                                                                                                
 def CLearScreen():
     print("\n" * 100)
 
@@ -35,38 +40,73 @@ def IsNumber(str_Number):
         if temp == False: return False
     return True
 
-def PrintError(error_name):
-    Error(error_name)
-    CLearScreen()
-    Introduction()
+def AddEmployee():
+    def GrossSalary(work_Salary, salary_Rate):
+        return work_Salary * salary_Rate
+
+    def Main():
+        name = input("Enter your Name: ")
+        position = input("Enter your Position: ")
+
+        ID = input("Enter your ID (6 digits only): ") 
+        if not IsNumber(ID):
+            PrintError("Number ERROR")
+            Main()
+            return False
+        
+        work_Salary = input("Enter your Work Salary: ") 
+        if not IsNumber(work_Salary): 
+            PrintError("Number ERROR")
+            Main()
+            return False
+
+        salary_Rate = input("Enter your Salary Rate: ") 
+        if not IsNumber(salary_Rate):
+            PrintError("Number ERROR")
+            Main()
+            return False
+
+        work_Salary = int(work_Salary)
+        salary_Rate = int(salary_Rate)
+        ID = int(ID)
+        
+        return True
     Main()
 
-def GrossSalary(work_Salary, salary_Rate):
-    return work_Salary * salary_Rate
+def RemoveEmployee():
+    pass
 
-def Main():
-    name = input("Enter your Name: ")
-    position = input("Enter your Position: ")
+def RetrieveEmployee():
+    pass
 
-    ID = input("Enter your ID (6 digits only): ") # checks if only contains integer
-    if not IsNumber(ID):
-        PrintError("Number ERROR")
-        return False
-    
-    work_Salary = input("Enter your Work Salary: ") 
-    if not IsNumber(work_Salary): # checks if only contains integer
-        PrintError("Number ERROR")
-        return False
+def EditEmployee():
+    pass
 
-    salary_Rate = input("Enter your Salary Rate: ") # checks if only contains integer
-    if not IsNumber(salary_Rate):
-        PrintError("Number ERROR")
-        return False
+def AdminChoice():
+    choice = input("""
+1) Add Employee
+2) Remove Employee
+3) Retrieve Employee Data
+4) Edit Employee Data
 
-    work_Salary = int(work_Salary)
-    salary_Rate = int(salary_Rate)
-    ID = int(ID)
-    
-    return True
-#Program Starts
-Main()
+Enter: """)
+    if IsNumber(choice):
+        choice = int(choice)
+        if choice == 1:
+            AddEmployee()
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        else:
+            PrintError("Only digits 1-4 only")
+            AdminChoice()
+    else:
+        PrintError("Wrong input!! Please Try Again")
+        AdminChoice()
+        
+        
+
+AdminChoice()
