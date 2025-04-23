eba31b9bf69b7bbff3b46dde46245d8d1a28d101
Joshua Ruiz <bossmasster100@gmail.com>
2025-04-23 16:37:54 +0800

    add functionality of code

--- employee.py
+++ employee.py
@@ -6,28 +6,50 @@ def Introduction():
     ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝      ╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║
     ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗    ███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║
     ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝    ╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝
-                                                                                                                                
     """  
     print(introduction)
 
-file = open("Data.txt", "w")
-file.write("12345,joshua,software dev,20000,100\n")
-file.close()
+def CLearScreen():
+    print("\n" * 100)
 
+def Error(error_Name):
+    print("_________________________________________________________________________________________________")
+    print(f"\n   {error_Name}!! Press Enter to try again")
+    error = input("_________________________________________________________________________________________________\n")
 
-name = ID = role = work_salary = salary_rate = ""
-file = open("Data.txt", "r")
-for line in file:
-    info = line.strip().split(",")
-    if info[0] == "12345":
-        name = info[0]
-        ID = info[1]
-        role = info[2]
-        work_salary = info[3]
-        salary_rate = info[4]
-file.close()
+# Execution
+CLearScreen()
+Introduction()
 
-gross_salary = 8 * salary_rate
-net_salary = gross_salary - (gross_salary * 4 / 100)
+while True:
+    employee_ID = input("Enter Employee's ID (6-digits only): ")
+    if len(employee_ID) != 6:
+        Error("Error!! It might be not enough length or employee does not exists")
+        CLearScreen()
+        Introduction()
+        continue
 
+    # Initialize default state
+    found = False
+    file = open("Data.txt", "r")
+    for line in file:
+        info = line.strip().split(",")
+        if info[0] == employee_ID:
+            ID = info[0]
+            name = info[1]
+            role = info[2]
+            work_salary = info[3]
+            salary_rate = info[4]
+            found = True
+            break
+    file.close()
 
+    if not found:
+        print("696!! Employee Not Found!")
+    else:
+        print(f"Name: {name}")
+        print(f"ID: {ID}")
+        print(f"Role: {role}")
+        print(f"Work Salary: {work_salary}")
+        print(f"Salary Rate: {salary_rate}")
+    break
